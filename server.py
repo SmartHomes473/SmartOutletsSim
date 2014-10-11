d5098769961cf75b57a50bacf70997fa708900d1
Nick Jurgens <njurgens@umich.edu>
2014-10-11 15:44:19 -0400

    Add compile python objects to .gitignore

--- server.py
+++ server.py
@@ -8,40 +8,53 @@
 __license__ = 'GPL-2.0'
 
 from bluetooth import *
-
-server_sock = BluetoothSocket(RFCOMM)
-server_sock.bind(('', 22))
-server_sock.listen(1)
-
-port = server_sock.getsockname()[1]
+import api
+import sim.coordinator
+import api.smartoutlets
 
 # UUID of Bluetooth Serial Port Profile
 uuid = '00001101-0000-1000-8000-00805F9B34FB'
+rfcomm_channel = 22
 
-# Register our service
-advertise_service( server_sock, "SampleServer",
-        service_id = uuid,
-        service_classes = [ uuid, SERIAL_PORT_CLASS ],
-        profiles = [ SERIAL_PORT_PROFILE ]
-)
+if __name__ == '__main__':
+    # initialize the simulator
+    simulator = sim.coordinator.Simulator()
+    api_parser = api.smartoutlets.APIParser(simulator)
 
-print('Waiting for connection on RFCOMM channel %d' % port)
+    # set up the Bluetooth server
+    server_sock = BluetoothSocket(RFCOMM)
+    server_sock.bind(('', rfcomm_channel))
+    server_sock.listen(1)
+    port = server_sock.getsockname()[1]
 
-while True:
-    client_sock, client_info = server_sock.accept()
-    print('Accepted connecton from ', client_info)
+    # Register our service
+    advertise_service( server_sock, "SampleServer",
+            service_id = uuid,
+            service_classes = [ uuid, SERIAL_PORT_CLASS ],
+            profiles = [ SERIAL_PORT_PROFILE ]
+    )
 
-    try:
-        while True:
-            data = client_sock.recv(1024)
-            if len(data) == 0:
-                break
-            print('received [%s]' % data)
-    except IOError:
-        pass
+    print('Waiting for connection on RFCOMM channel %d' % port)
 
-    print('disconnected')
-    client_sock.close()
+    while True:
+        client_sock, client_info = server_sock.accept()
+        print('Accepted connecton from ', client_info)
 
-server_sock.close()
-print('all done')
+        try:
+            while True:
+                data = client_sock.recv(1024)
+
+                # end of connection
+                if len(data) == 0:
+                    break
+
+                api_parser.respond(data)
+
+        except IOError:
+            pass
+
+        print('disconnected')
+        client_sock.close()
+
+    server_sock.close()
+    print('all done')
